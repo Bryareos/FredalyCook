fdf2f4f1d5fe7aea193c7b60dd98e0347bfdf2de
Bryareos <david-o@jhiki.fr>
2020-11-19 00:03:04 +0100

    Design homepage

---
+++ src/Controller/MainController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class MainController
+ * @package App\Controller
+ * @Route("/")
+ */
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     * @param RecipeRepository $repo
+     * @return Response
+     */
+    public function index(RecipeRepository $repo): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'recipes' => $repo->findTheBestHeightRecipes(),
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig');
+    }
+
+    /**
+     * @return Response
+     * @Route("/search", name="search")
+     */
+    public function search(): Response
+    {
+        return $this->render('main/search.html.twig');
+    }
+}
